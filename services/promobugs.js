44aebf0fb854df7a74d85bf91272bfeb4ba15de5
Guilherme Teodoro <guilherme.teodoro@apontador.com>
2015-10-02 17:22:17 -0300

    add if xbox

--- services/promobugs.js
+++ services/promobugs.js
@@ -12,12 +12,14 @@ var promobugsService = {
         var results = [];  
 
         $(promobugs.listItemQuery).each(function() {
-          results.push({
-            title: $(this).find(promobugs.title.query).text(),
-            link: window.document.location.origin + $(this).find(promobugs.link.query).attr(promobugs.link.attr),
-            threadId: $(this).attr('id'),
-            date: new Date($(this).find(promobugs.date.query).attr(promobugs.date.attr))
-          });
+          if($(this).find(promobugs.title.query).text().toLowerCase().match(/xbox/g)) {
+            results.push({
+              title: $(this).find(promobugs.title.query).text(),
+              link: window.document.location.origin + $(this).find(promobugs.link.query).attr(promobugs.link.attr),
+              threadId: $(this).attr('id'),
+              date: new Date($(this).find(promobugs.date.query).attr(promobugs.date.attr))
+            });
+          }
         });
 
         resolve(results);
